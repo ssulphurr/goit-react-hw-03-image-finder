346b888503b6fda8af9926138f7b7a0f6ad79212
ssulphurr <ssulphurr@gmail.com>
2023-05-16 18:56:06 +0300

    corrects linting error

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import css from './ImageGallery.module.css';
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
